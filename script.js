aef230c9d19e57a1624ae65d8018665a38b49334
alaa-abdoh <alaajabdoh1701@gmail.com>
2023-07-26 15:16:07 +0300

    make the header fixed with scroll

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function handleList(){
     let nav = document.querySelector("header .container nav");
-    nav.style.top == "-100%" ? nav.style.top ="65px" :  nav.style.top = "-100% " 
+    nav.style.top == "-1000px" ? nav.style.top ="65px" :  nav.style.top = "-1000px" 
 }
 
 
